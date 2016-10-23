16744ae7563cea3026119297d10b718633d4e00d
karanba <altaykarakus@gmail..com>
2016-10-23 05:59:41 +0300

    working with dırectıon errors

--- Uno/RF/RF_Basics_Alici/RF_Basics_Alici.ino
+++ Uno/RF/RF_Basics_Alici/RF_Basics_Alici.ino
@@ -7,7 +7,12 @@
 int msg[2];
 RF24 radio(9, 10);
 const uint64_t pipe = 0xE8E8F0F0E1LL;
-int LED1 = 8;
+
+int MOTORA_PWM = 5;
+int MOTORA_DIR = 4;
+int MOTORB_PWM = 3;
+int MOTORB_DIR = 2;
+int x = 0, y = 0, v = 0, l = 1, r = 1;
 
 void setup(void) {
   Serial.begin(9600);
@@ -15,24 +20,64 @@ void setup(void) {
   radio.begin();
   radio.openReadingPipe(1, pipe);
   radio.startListening();
-  pinMode(LED1, OUTPUT);
+  pinMode(MOTORA_DIR, OUTPUT);
+  pinMode(MOTORB_DIR, OUTPUT);
+  pinMode(MOTORA_PWM, OUTPUT);
+  pinMode(MOTORB_PWM, OUTPUT);
+  digitalWrite(MOTORA_DIR, LOW);
+  digitalWrite(MOTORB_DIR, LOW);
+  digitalWrite(MOTORA_PWM, LOW );
+  digitalWrite(MOTORB_PWM, LOW );
 }
 
 void loop(void) {
-  while (radio.available()) {
-    radio.read(msg, 2);
 
-    if (msg[0] > 500) {
-      digitalWrite(LED1, HIGH);
-      delay(300);
-    }
-    Serial.print("X: ");
-    Serial.println(msg[0]);
+  while (radio.available()) {
+    radio.read(msg, sizeof(msg));
+    x = map(msg[0], 0, 1023, -255, 255);
+    y = map(msg[1], 0, 1023, -100, 100);
+ Serial.print("X: ");
+    Serial.println(x);
 
     Serial.print("Y: ");
-    Serial.println(msg[1]);
+    Serial.println(y);
+
+
+    if (x > -10 && x < 10) {
+      digitalWrite(MOTORA_DIR, LOW);
+      digitalWrite(MOTORB_DIR, LOW);
+      digitalWrite(MOTORA_PWM, LOW );
+      digitalWrite(MOTORB_PWM, LOW );
+      break;
+    } else if (x < -10) {
+      digitalWrite(MOTORA_DIR, HIGH);
+      digitalWrite(MOTORB_DIR, HIGH);
+     
+    } else {
+      digitalWrite(MOTORA_DIR, LOW);
+      digitalWrite(MOTORB_DIR, LOW);
+    
+    }
+
+
+    if (y > 20) {
+      l = 0; r = 1;
+     
+    } else if (y < -25) {
+      l = 1; r = 0;
+      
+    } else {
+      l = 1; r = 1;
+    }
+   
+    Serial.print("l: ");
+    Serial.println(l);
+    Serial.print("r: ");
+    Serial.println(r);
+    analogWrite(MOTORB_PWM, x*l);
+    analogWrite(MOTORA_PWM, x*r);
+
   }
 
-  digitalWrite(LED1, LOW);
   delay(100);
 }
